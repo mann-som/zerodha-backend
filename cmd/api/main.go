ec74391d8c5837d8fe00cefca3ca75d250540b4c
Mann <mann.som@reconnectenergy.com>
2025-08-16 23:04:55 +0530

    Initial-commit

---
+++ cmd/api/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/mann-som/zerodha-backend/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mann-som/zerodha-backend
+
+go 1.13
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/sirupsen/logrus v1.9.3
+)
